53f1563c16e3e52f5fd45a103ee81ba5eb399307
Per Rawdin <perrawd@gmail>
2023-01-01 21:13:58 +0100

    Initial commit

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { ReportHandler } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    import('web-vitals').then(
+      ({
+        getCLS, getFID, getFCP, getLCP, getTTFB,
+      }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      },
+    );
+  }
+};
+
+export default reportWebVitals;
